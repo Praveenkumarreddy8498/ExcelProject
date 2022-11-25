703279ee361fbc8445a5650962dc57b0d49fce54
PraveenKumarReddy <praveenkumarreddy8498@gmail.com>
2022-11-25 18:58:54 +0530

    First commit

--- schedule-cloud-gateway/src/main/java/com/excel/ScheduleCloudGatewayApplication.java
+++ schedule-cloud-gateway/src/main/java/com/excel/ScheduleCloudGatewayApplication.java
@@ -1,12 +1,7 @@
 package com.excel;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
